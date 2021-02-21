8971d79faa66543ce7fcdb5be0c2803aeed18c82
dlee-171 <dlee171@my.centennialcollege.ca>
2021-02-21 17:06:02 -0500

    Removed API & Added migration to api web

--- WeLinkUp/WeLinkUp/Startup.cs
+++ WeLinkUp/WeLinkUp/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using WeLinkUp.Data;
 
 namespace WeLinkUp
 {
@@ -23,6 +25,7 @@ namespace WeLinkUp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews();
         }
 
