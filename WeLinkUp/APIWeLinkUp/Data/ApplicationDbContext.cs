fe119a6040d39f02f39c7a427c7e0faa40707fdb
dlee-171 <dlee171@my.centennialcollege.ca>
2021-02-25 18:20:13 -0500

    Revert "update homepage"
    This reverts commit b66b17624988a5ef98b1328aad3a257b5df23e88.

---
+++ WeLinkUp/APIWeLinkUp/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using APIWeLinkUp.Models;
+
+namespace APIWeLinkUp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
