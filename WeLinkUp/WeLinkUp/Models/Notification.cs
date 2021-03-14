0e57b6571fd12fd46977c5d37959af707d503c60
dlee-171 <dlee171@my.centennialcollege.ca>
2021-03-14 17:21:44 -0400

    fixed issue

--- WeLinkUp/WeLinkUp/Models/Notification.cs
+++ WeLinkUp/WeLinkUp/Models/Notification.cs
@@ -16,6 +16,7 @@ namespace WeLinkUp.Models
         public string SenderId { get; set; }
         public string Message { get; set; }
         public int EventId { get; set; }       
-        public DateTime Date { get; set; }
+        
+        public string NotificationDate { get; set; }
     }
 }
