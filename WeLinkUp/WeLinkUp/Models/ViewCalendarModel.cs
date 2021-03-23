b46a4099eb25706511b0427743da8270ec723c1d
dlee-171 <dlee171@my.centennialcollege.ca>
2021-03-23 09:28:11 -0400

    Create ViewCalendarModel.cs

---
+++ WeLinkUp/WeLinkUp/Models/ViewCalendarModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeLinkUp.Models
+{
+    public class ViewCalendarModel
+    {
+        public int CalendarId { get; set; }
+        public string UserId { get; set; }
+        public int EventId { get; set; }
+
+        public string EventTitle { get; set; }
+        public string Date { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+    }
+}
